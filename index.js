fe1bfe5c14b549e4b21470d2b50f2b172ac7d9e1
Jenny Ardrey <jenny@ardrey.co.uk>
2020-02-29 14:11:52 +0000

    prepared for deployment

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 const app = require('./src/app');
 
-const port = process.env.PORT;
-
+const PORT = process.env.PORT || 3000;
 mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true }, () => {
-  app.listen(port, () => {
+  app.listen(PORT, () => {
     // eslint-disable-next-line no-console
-    console.log('App listening on port 3000');
+    console.log('App listening on port', PORT);
   });
 });
