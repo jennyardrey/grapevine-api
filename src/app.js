22db0f2019081302574412360126a7e2aa8f9261
Jenny Ardrey <jenny@ardrey.co.uk>
2020-03-02 16:42:13 +0000

    added cors to dependencies so endpoints can be used in react app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const userControllers = require('./controllers/user');
 const moodControllers = require('./controllers/mood');
 
 const app = express();
+app.use(cors({ credentials: true, origin: true }));
+app.options('*', cors());
 app.use(express.json());
 
 app.post('/user', userControllers.create);
@@ -11,6 +14,6 @@ app.post('/user/mood', moodControllers.postMood);
 app.post('/user/message', moodControllers.postMessage);
 app.get('/moods', moodControllers.getMoods);
 // app.get('/moods/:role', moodControllers.getRoleMoods);
-app.get('/moods/messages/', moodControllers.getMessages);
+app.get('/moods/messages', moodControllers.getMessages);
 
 module.exports = app;
